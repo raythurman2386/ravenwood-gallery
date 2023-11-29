1c9d466f499d3752653fcd4b50fcd0ed0a74e5e8
raythurman2386 <raymondthurman5@gmail.com>
2023-11-29 13:35:43 -0600

    updated image

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,11 +34,11 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         <title>Ravenwood AI Gallery</title>
         <meta
           property="og:image"
-          content="https://nextjsconf-pics.vercel.app/og-image.png"
+          content="https://ravenwood-gallery.vercel.app/og-image.png"
         />
         <meta
           name="twitter:image"
-          content="https://nextjsconf-pics.vercel.app/og-image.png"
+          content="https://ravenwood-gallery.vercel.app/og-image.png"
         />
       </Head>
       <main className="mx-auto max-w-[1960px] p-4">
