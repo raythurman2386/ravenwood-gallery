318b6a420e62319733dbbbb18da47824088144c2
raythurman2386 <raymondthurman5@gmail.com>
2023-12-02 21:59:11 -0600

    updated shop button

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,9 +69,11 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             </p>
             <Link
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
-              href="#"
+              href="https://ravenwood-creations.printify.me/products"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              Sign Up Available Soon
+              Visit Store
             </Link>
           </div>
           {images.map(({ id, public_id, format, blurDataUrl }) => (
